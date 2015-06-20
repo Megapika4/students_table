8317a51e7c8d9f96ed8fb8edb39b99143f9133ef
Megapika4 <kaktyc3d@mail.ru>
2015-06-20 13:10:29 +0300

    Revert "init files"
    This reverts commit 6c9ee352d0724da1ce85ec1e40b7a788c0322c17.

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require_once "lib/controller_class.php"; //подключение контроллера
-	
-	$controller = new Controller();
-
-	$page = $controller->getPage("index"); //формирует главную страницу
-		
-	echo $page;
